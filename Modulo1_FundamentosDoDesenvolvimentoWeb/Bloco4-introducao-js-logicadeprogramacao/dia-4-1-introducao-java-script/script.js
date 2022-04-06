132db727bf297cd128d88aa660a22770a4119839
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2022-04-06 15:53:04 -0300

    até o exercício 4

--- Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-1-introducao-java-script/script.js
+++ Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-1-introducao-java-script/script.js
@@ -1,15 +1,35 @@
-let estadoCanditato = "Gabriel";
+const valor = 0;
 
-switch (estadoCanditato) {
-    case "aprovada":
-        console.log("Parabéns");
-        break;
-    case "lista":
-        console.log("Está na lista de espera");
-        break;
-    case "reprovada":
-            console.log("Reprovada, tente novamennte.");
-            break;
-    default:
-        console.log("valor não identificado");
+if (valor > 0) {
+    console.log("positive");
+} else if (valor < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
 }
+
+// const a = 50;
+// const b = 50;
+// const c = 50;
+
+// if (a > b && a > c) {
+//     console.log("a é maior que b e c");
+// } else if (b > a && b > c) {
+//     console.log("b é maior que a e c");
+// } else if (c > a && c > b) {
+//     console.log("c é maior que a e b");
+// } else {
+//     console.log("os três números possuem o mesmo valor")
+// }
+
+// let adicao = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a * b;
+// let divisao = a / b;
+// let modulo = a % b;
+
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
