a9e0d96c1770a943b8b261e9e69ae0942136ffc9
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2022-04-07 11:17:20 -0300

    Primeiro exercício

---
+++ Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,30 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let numeros of numbers) {
+    console.log(numeros);
+}
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// let word = "Simpsons"
+
+// for(let nomes of word) {
+//     console.log(nomes)
+// }
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index += 1) {
+//     console.log(groceryList[index]);
+// }
+
+// let listaDeNomes = ["Joana", "Maria", "Gabriella", "Joaquina"]
+
+// for (let indice = 0; indice < listaDeNomes.length; indice++) {
+//     console.log(listaDeNomes[indice])
+// }
+
+// let numero = 3;
+
+// for(let contador = 1; contador <= 9; contador++) {
+//     console.log(numero * contador);
+// }
