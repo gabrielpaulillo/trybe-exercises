dd0497105dccbfc0bebd8911d2536597f11c6c00
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2022-04-07 18:02:50 -0300

    Falhei no exercício

--- Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-3-logica-de-programacao-e-algoritmos/script.js
+++ Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-3-logica-de-programacao-e-algoritmos/script.js
@@ -1,7 +1,31 @@
-const n = 5;
+let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-let resultado = 0;
-for (let i = 0; i <= n; i += 1) {
-    resultado =+ i;
+for (let i = 0; i < array.length; i += 1) {
+    for (let k = i + 1; k < array.length; k += 1){
+        if(array[i] < array[k]) {
+            let j = array[i];
+            array[i] = array[k];
+            array[k] = j;
+        }
+    }
+    console.log(array[i]);
+
 }
-console.log(resultado);
+
+//Exercicio 2 - aula 4.3
+// let word = 'paralelepipedo';
+
+// for (let index = word.length - 1 ; index >= 0; index -= 1) {
+//     console.log(word[index]);
+// }
+//Referência reverse loop: https://www.techiedelight.com/loop-through-array-backwards-javascript/
+
+//Exercício 1 - dia 4.3
+// const fatorial = [10,9,8,7,6,5,4,3,2,1];
+
+// let resultado = 1;
+// for (let i = 0; i < fatorial.length; i++) {
+//     resultado *= fatorial[i];
+// }
+
+// console.log(resultado);
