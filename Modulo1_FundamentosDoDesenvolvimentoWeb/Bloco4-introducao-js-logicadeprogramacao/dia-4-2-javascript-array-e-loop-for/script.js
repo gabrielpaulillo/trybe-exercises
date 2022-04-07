2146ece34d8794d15d28a5e566d9d546e054b86e
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2022-04-07 11:48:36 -0300

    Exercício 8 feito dia 4.2 finalizado

--- Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-2-javascript-array-e-loop-for/script.js
+++ Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-2-javascript-array-e-loop-for/script.js
@@ -1,8 +1,75 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let numeros of numbers) {
-    console.log(numeros);
-}
+
+//Exercício 8 - dia4.2
+// let listaNumeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
+//                     17,18,19,20,21,22,23,24,25]
+
+//     // for (let index = 0; index < listaNumeros.length; index +=1) {
+//     //     console.log(listaNumeros[index]);
+//     // }
+//     for(let numeros of listaNumeros) {
+//         console.log(numeros);
+//     }
+
+
+// Exercicio 7 - dia4.2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 100;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if(resultado < numbers[index]) {
+//         resultado = resultado;
+//     } else if (resultado > numbers[index]) {
+//         resultado = numbers[index];
+//     } else {
+//         resultado = resultado;
+//     }
+// }
+// console.log(resultado);
+
+//Exercicio 6 - dia4.2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if(numbers[index] % 2 != 0) {
+//         console.log(numbers[index]);
+//     } else {
+//         console.log("Nenhum valor ímpar encontrado");
+//     }
+// }
+
+//Exercicio 5 - dia4.2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if(resultado > numbers[index]) {
+//         resultado = resultado;
+//     } else if (resultado < numbers[index]) {
+//         resultado = numbers[index];
+//     } else {
+//         resultado = resultado;
+//     }
+// }
+// console.log(resultado);
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// // .reduce((partialSum, a) => partialSum +a, 0);
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     // for (let numeros of numbers) {
+//     //     numeros += numbers[index];
+        
+//     // }
+//     resultado += numbers[index];
+// }
+// resultado /= numbers.length;
+// console.log(resultado);
+
+// console.log(numbers);
+
 
 // let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 // let word = "Simpsons"
