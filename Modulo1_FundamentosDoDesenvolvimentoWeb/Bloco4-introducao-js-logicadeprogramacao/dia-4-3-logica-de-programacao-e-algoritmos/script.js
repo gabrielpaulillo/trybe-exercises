3c2f7f6624f25837de3dfa28c38a780bde18dc38
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2022-04-08 11:13:05 -0300

    commitando

---
+++ Modulo1_FundamentosDoDesenvolvimentoWeb/Bloco4-introducao-js-logicadeprogramacao/dia-4-3-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,29 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = "";
+
+for (let i = 0; i < array.length; i += 1) {
+    for (let k = i + 1; k < array.length; k += 1){
+        if(array[i] < array[k]) {
+            
+        }
+    }
+    console.log(array[i]);
+}
+
+//Exercicio 2 - aula 4.3
+// let word = 'paralelepipedo';
+
+// for (let index = word.length - 1 ; index >= 0; index -= 1) {
+//     console.log(word[index]);
+// }
+//Referência reverse loop: https://www.techiedelight.com/loop-through-array-backwards-javascript/
+
+//Exercício 1 - dia 4.3
+// const fatorial = [10,9,8,7,6,5,4,3,2,1];
+
+// let resultado = 1;
+// for (let i = 0; i < fatorial.length; i++) {
+//     resultado *= fatorial[i];
+// }
+
+// console.log(resultado);
